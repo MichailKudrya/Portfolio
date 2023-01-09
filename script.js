93e9e4744f7cae79bca304407aca858002be821b
MichailKudrya <miget221@gmail.com>
2023-01-09 18:48:24 +0200

    scrooll

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ for (let anchor of anchors) {
   anchor.addEventListener("click", function(event) {
     event.preventDefault();
     const blockID = anchor.getAttribute('href')
-    document.querySelector("" + blockID).scrollIntoView({
+    document.querySelector(" " + blockID).scrollIntoView({
       behavior: "smooth",
       block: "start"
     })
